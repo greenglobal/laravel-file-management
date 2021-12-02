e442592f8ecf0bb398e1c50a832efa7c734255e8
Nguyen Duy Nguyen <keezimin@gmail.com>
2021-12-02 21:04:54 +0700

    Latest code

--- config/file-media.php
+++ config/file-media.php
@@ -37,8 +37,8 @@ $disksConfig = [
     ],
 ];
 
+
 $configs = config('filesystems');
-dd($configs['disks']);
 foreach ($listDisks as $disk) {
     if (array_key_exists($disk, $configs['disks'])) {
         unset($disksConfig[$disk]);
@@ -57,83 +57,29 @@ return [
         'NOT_ACCEPTABLE'       => 406,
         'SUCCESS'              => 200,
     ],
-
+    
     'disks' => $disksConfig,
-
+    
     'list_disk' => $listDisks,
-
+    
     'name_generator' => env('NAME_GENERATE', false),
-
+    
     /*
      * The disk on which to store added files and derived images by default. Choose
      * one or more of the disks you've configured in config/filesystems.php.
      */
     'disk_name' => env('STORAGE_DISK', 'local'),
+    'folder_save' => env('FOLDER_SAVE', 'library'),
+    
+    'watermark' => env('WATERMARK', 'false'),
+    'watermark_test' => env('WATERMARK_TEXT', 'Watermark'),
 
-    /*
-     * The maximum file size of an item in bytes.
-     * Adding a larger file will result in an exception.
-     */
-    'max_file_size' => 1024 * 1024 * 10,
+    'thumbnail' => env('THUMBNAIL', 'true'),
+    'thumbnail_size' => env('THUMBNAIL_SIZE', '30,30'),
+    'thumbnail_storage' => env('THUMBNAIL_STORAGE', 'thumbnails'),
+    
+    'optimize_image' => env('OPTIMIZE_IMAGE', 'true'),
+    'rotate_image' => env('ROTATE_IMAGE', 'true'),
+    'rotate_deg' => env('ROTATE_DEG', '-90'),
 
-    /*
-     * The media library will try to optimize all converted images by removing
-     * metadata and applying a little bit of compression. These are
-     * the optimizers that will be used by default.
-     */
-    'image_optimizers' => [
-        Spatie\ImageOptimizer\Optimizers\Jpegoptim::class => [
-            '--strip-all', // this strips out all text information such as comments and EXIF data
-            '--all-progressive', // this will make sure the resulting image is a progressive one
-        ],
-        Spatie\ImageOptimizer\Optimizers\Pngquant::class => [
-            '--force', // required parameter for this package
-        ],
-        Spatie\ImageOptimizer\Optimizers\Optipng::class => [
-            '-i0', // this will result in a non-interlaced, progressive scanned image
-            '-o2', // this set the optimization level to two (multiple IDAT compression trials)
-            '-quiet', // required parameter for this package
-        ],
-        Spatie\ImageOptimizer\Optimizers\Svgo::class => [
-            '--disable=cleanupIDs', // disabling because it is known to cause troubles
-        ],
-        Spatie\ImageOptimizer\Optimizers\Gifsicle::class => [
-            '-b', // required parameter for this package
-            '-O3', // this produces the slowest but best results
-        ],
-    ],
-
-    /*
-     * These generators will be used to create an image of media files.
-     */
-    'image_generators' => [
-        Spatie\MediaLibrary\Conversions\ImageGenerators\Image::class,
-        Spatie\MediaLibrary\Conversions\ImageGenerators\Webp::class,
-        Spatie\MediaLibrary\Conversions\ImageGenerators\Pdf::class,
-        Spatie\MediaLibrary\Conversions\ImageGenerators\Svg::class,
-        Spatie\MediaLibrary\Conversions\ImageGenerators\Video::class,
-    ],
-
-    /*
-     * The engine that should perform the image conversions.
-     * Should be either `gd` or `imagick`.
-     */
-    'image_driver' => env('IMAGE_DRIVER', 'gd'),
-
-    /*
-     * FFMPEG & FFProbe binaries paths, only used if you try to generate video
-     * thumbnails and have installed the php-ffmpeg/php-ffmpeg composer
-     * dependency.
-     */
-    'ffmpeg_path' => env('FFMPEG_PATH', '/usr/bin/ffmpeg'),
-    'ffprobe_path' => env('FFPROBE_PATH', '/usr/bin/ffprobe'),
-
-    /*
-     * Here you can override the class names of the jobs used by this package. Make sure
-     * your custom jobs extend the ones provided by the package.
-     */
-    'jobs' => [
-        'perform_conversions' => \Spatie\MediaLibrary\Conversions\Jobs\PerformConversionsJob::class,
-        'generate_responsive_images' => \Spatie\MediaLibrary\ResponsiveImages\Jobs\GenerateResponsiveImagesJob::class,
-    ],
 ];
